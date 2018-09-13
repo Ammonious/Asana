97d73bc44f32375cb5e83d66ec668672ad37040c
Corbin Matschull <corbin@jux.moe>
2018-09-13 15:49:25 -0600

    Remove unneeded section

--- webview.js
+++ webview.js
@@ -1,9 +1,7 @@
 module.exports = (Franz, options) => {
   function getTasks() {
 
-
     const todayElement = document.querySelectorAll(".dropTargetRow").length;
-  //  const inboxElement = document.querySelector("#filter_inbox .counter");
 
     Franz.setBadge(todayElement);
   }
